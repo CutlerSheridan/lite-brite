7ece861fe6dced63f88503b04e60bd50ea67396c
Cutler Sheridan <cutler.sheridan@gmail.com>
2021-07-21 16:40:06 -0700

    Add left buttons + clear

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 Move your cursor around to light up the board.
 
 TO-DO
--change color + fill on hover
+~~-change color + fill on hover~~
 -specify grid size (up to 100?)
 -change to specific color
 -change to random color
--clear pegs
+~~-clear pegs~~
 -turn light on/off
+-add something so the lights go off for a moment before getting taken out completely
+-make the size of the pegs variable based on the number
 -make each pass over the same peg brighter?  like add x% each time
+-color picker doesn't end party
+-optimize?
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,38 @@
 const gridContainer = document.querySelector(".grid-container");
-let reset = false;
-let pegBase = 10;
+let pegBase = 25;
+let pegHoles;
+let litColor = "rgb(255,0,0)";
+let unlitColor;
+setUnlitColor(litColor);
+let partyIsOn = false;
+let lightIsOn = true;
+let placedPegs = [];
 
 createGrid();
 const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", clearBoard);
+
+const randomColorBtn = document.querySelector("#random-color");
+randomColorBtn.addEventListener("click", randomizeColor);
+
+const partyBtn = document.querySelector("#party-btn");
+partyBtn.addEventListener("click", () => {
+    partyIsOn = true;
+    const allBtns = document.querySelectorAll(".btn");
+    allBtns.forEach(btn => btn.addEventListener("click", endParty));
+});
+
+const lightSwitch = document.querySelector("#light-switch");
+lightSwitch.addEventListener("click", toggleLight);
+
+const colorPicker = document.getElementById("color-picker")
+colorPicker.addEventListener("change", (e) => {
+    litColor = `rgb(${convertHexToRgbNums(e.target.value)}`;
+    setUnlitColor(litColor);
+    console.log(litColor);
+});
+
+//colorPicker.addEventListener("click", () => {alert(e.target.value)});
 
 // LOGIC FUNCTIONS START
 function createGrid() {
@@ -15,35 +43,127 @@ function createGrid() {
         gridContainer.appendChild(cell);
     }
     gridContainer.style.gridTemplate = `repeat(${pegBase}, 1fr) / repeat(${pegBase}, 1fr)`;
-    toggleListenersOnHoles();
+    pegHoles = document.querySelectorAll(".peg-hole");
+    addListenersToHoles();
 }
 
-function toggleListenersOnHoles() {
-    const pegHoles = document.querySelectorAll(".peg-hole");
-    if (reset) {
-        pegHoles.forEach(peg => {
-            peg.classList.remove("peg-lit");
-            peg.classList.remove("peg-unlit");
-        });
-        reset = false;
-    } else {
-        pegHoles.forEach(peg => {
-            peg.addEventListener("mouseenter", insertPeg);
-            peg.addEventListener("mouseleave", lightPeg);
-        });
-    }
+function addListenersToHoles() {
+    pegHoles.forEach(peg => {
+        peg.addEventListener("mouseenter", insertPeg, {once: true});
+        peg.addEventListener("mouseleave", lightPeg, {once: true});
+    });
 }
 
 function insertPeg() {
-    this.classList.add("peg-unlit");
+    // conditional so hovering over lit pegs doesn't unlight them
+    if (this.classList.contains("peg-lit")) {
+        return;
+    } else {
+        if (partyIsOn) {
+            randomizeColor();
+        }
+        unlight(this);
+        placedPegs.push(this);
+    }
+}
+function unlight(peg) {
+    peg.style.backgroundColor = unlitColor;
+    peg.style.borderColor = unlitColor;
+    peg.classList.add("peg-unlit");
+    peg.style.boxShadow = "none";
 }
 function lightPeg() {
-    this.classList.add("peg-lit");
+    if (lightIsOn) {
+        this.classList.remove("peg-unlit");
+        this.style.backgroundColor = litColor;
+        this.style.borderColor = litColor;
+        this.style.boxShadow = `0 0 .8rem .5rem ${litColor}`;
+        // if I decide to get rid of the grey circle on the lit pegs, re-add
+        // this.style.backgroundClip = "border-box";
+        this.classList.add("peg-lit");
+    }
+}
+function light(peg) {
+    if (lightIsOn) {
+        peg.classList.remove("peg-unlit");
+        peg.style.backgroundColor = litColor;
+        peg.style.borderColor = litColor;
+        peg.style.boxShadow = `0 0 .8rem .5rem ${litColor}`;
+        // if I decide to get rid of the grey circle on the lit pegs, re-add
+        // this.style.backgroundClip = "border-box";
+        peg.classList.add("peg-lit");
+    }
 }
 
 function clearBoard() {
-    reset = true;
-    toggleListenersOnHoles();
+    placedPegs.forEach(peg => {
+        peg.classList.remove("peg-lit");
+        peg.style.borderColor = "transparent";
+        peg.style.backgroundClip = "content-box";
+        peg.style.backgroundColor = "black";
+        peg.style.boxShadow = "none";
+        peg.classList.remove("peg-unlit");
+    });
+    placedPegs = [];
+    addListenersToHoles();
+}
+
+function randomizeColor() {
+    let newColorNums = `${random(361)},${random(361)},${random(361)}`;
+    litColor = `rgb(${newColorNums})`;
+    setUnlitColor(litColor);
+}
+function extractColorNums(rgbValue) {
+    let r, g, b;
+    rgbValue = rgbValue.slice(rgbValue.indexOf("(") + 1, rgbValue.length - 1);
+    let rgbArray = rgbValue.split(",");
+    rgbArray.forEach(num => num.trim());
+    r = rgbArray[0];
+    g = rgbArray[1];
+    b = rgbArray[2];
+    // this works if passed rgb OR rgba
+    return `${r},${g},${b}`;
+}
+function setUnlitColor(rgbValue) {
+    unlitColor = `rgba(${extractColorNums(rgbValue)},.5)`;
+}
+function convertHexToRgbNums(hex) {
+    hex = hex.slice(1);
+    let hexArray = [
+        hex.substr(0, 2),
+        hex.substr(2, 2),
+        hex.substr(4, 2)
+    ];
+    let rgbArray = [
+        parseInt(hexArray[0], 16),
+        parseInt(hexArray[1], 16),
+        parseInt(hexArray[2], 16)
+    ];
+    console.log(rgbArray);
+    return `${rgbArray[0]},${rgbArray[1]},${rgbArray[2]}`;
+}
+
+function endParty() {
+    if (partyIsOn && this !== partyBtn) {
+        partyIsOn = false;
+    }
+}
+
+function toggleLight() {
+    placedPegs.forEach(peg => {
+        let pegColor = getComputedStyle(peg).backgroundColor;
+        if (peg.classList.contains("peg-lit")) {
+            lightIsOn = false;
+            peg.classList.remove("peg-lit");
+            setUnlitColor(pegColor);
+            console.log(pegColor);
+            unlight(peg);
+        } else if (peg.classList.contains("peg-unlit")) {
+            lightIsOn = true;
+            litColor = `rgb(${extractColorNums(pegColor)})`;
+            light(peg);
+        }
+    })
 }
 // LOGIC FUNCTIONS END
 
